4ab8082a4042e70210e09e2d035e35639e7915fe
Johnny <johnny@unstoppabledomains.com>
2019-09-11 01:11:25 -0700

    Updates

---
+++ precache-manifest.3503e72242131283569c305be8280b2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d58d36c873236cfa49e5957cee3708",
+    "url": "/unstoppabledocs/index.html"
+  },
+  {
+    "revision": "84126761b29159b5ad04",
+    "url": "/unstoppabledocs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "496d284bdda156be741d",
+    "url": "/unstoppabledocs/static/js/2.f14ad1b7.chunk.js"
+  },
+  {
+    "revision": "84126761b29159b5ad04",
+    "url": "/unstoppabledocs/static/js/main.bd26cead.chunk.js"
+  },
+  {
+    "revision": "e5b679b78d9baf26630b",
+    "url": "/unstoppabledocs/static/js/runtime~main.6d329362.js"
+  },
+  {
+    "revision": "59300d9f667d39ce0e1f450d7521d0d6",
+    "url": "/unstoppabledocs/static/media/spec.59300d9f.yaml"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.efc641c3ad32c40d619187c3a7649983.js"
+  "/unstoppabledocs/precache-manifest.3503e72242131283569c305be8280b2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/unstoppabledocs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
